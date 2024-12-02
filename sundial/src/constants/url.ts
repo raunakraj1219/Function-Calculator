217d1522aacfa8db40daf6556ca8dbdae524c6b6
Raunak Raj <44779389+raunakraj1219@users.noreply.github.com>
2024-12-02 16:39:33 +0530

    Delete sundial directory

--- sundial/src/constants/url.ts
+++
@@ -1,5 +0,0 @@
-export const AVAILABLE_METRICS =
-    "https://sundial-fe-interview.vercel.app/api/metrics";
-
-export const AVAILABLE_SEGMENTS =
-    "https://sundial-fe-interview.vercel.app/api/segments";
